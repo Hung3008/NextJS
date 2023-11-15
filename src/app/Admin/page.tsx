92a2a269a1ff4ad7bad158162a7f6c8f35cc782c
Hung3008 <hungpham.300801@gmail.com>
2023-11-15 17:58:01 +0700

    15/11

--- src/app/Admin/page.tsx
+++ src/app/Admin/page.tsx
@@ -3,15 +3,46 @@ import React from 'react';
 import Header from './components/header';
 import style1 from '../style/app.module.css'
 
-const HomePage = () => {
+export default function Home() {
     return (
-        <div className={style1['red']}>
-            <Header />
-            <span className={style1['black']}> <h1>Trang chủ</h1></span>
-            {/* Nội dung trang chủ */}
-        </div>
+        <main className='header'>
+            <div className={style1['top-header']}>
+                <div className={style1['top-header-inner']}>
+                    <div className={style1['container']}>
+                        <div className={style1['row align-items-center']}>
+                            <div className={style1['col-lg-6 d-none d-lg-block']}>
+                                <nav className={style1["header-navigation"]}>
+                                    <div className={style1["menu-secondary-menu-container"]}>
+                                        <ul id={style1["secondary-menu"]} className={style1["menu"]}>
+                                            <li id="menu-item-130" className={style1["menu-item menu-item-type-post_type menu-item-object-page menu-item-130"]}>
+                                                <a href="">TÀI KHOẢN</a></li>
+                                            <li id="menu-item-131" className={style1["menu-item menu-item-type-post_type menu-item-object-page menu-item-131"]}>
+                                                <a href="">YÊU THÍCH</a></li>
+                                            <li id="menu-item-132" className={style1["menu-item menu-item-type-post_type menu-item-object-page menu-item-132"]}>
+                                                <a href="">THANH TOÁN</a></li>
+                                            <li id="menu-item-460" className={style1["menu-item menu-item-type-post_type menu-item-object-page menu-item-460"]}>
+                                                <a href="">GIỎ HÀNG</a></li>
+                                        </ul>
+                                    </div>
+                                </nav>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <div className={style1['banner']}>
+
+            </div>
+
+
+        </main>
+
     );
+
 };
 
-export default HomePage;
+
 
